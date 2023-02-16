954624b36cd615b510af7fd42576d193676985cf
Abdurahmon <abdutuit@gmail.com>
2023-02-16 11:19:38 +0500

    update weather

--- data/weather_inf.py
+++ data/weather_inf.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-
+from PyQt6.QtGui import QIcon,QPixmap
 def get_weather(city):
-
-    weather=requests.get(f"https://sinoptik.ua/погода-{city}")
-    soup=BeautifulSoup(weather.content)
-    temp=soup.find("p",class_="today-temp").text
-
-    return temp
+   weather=requests.get(f"https://sinoptik.ua/погода-{city}")
+   soup=BeautifulSoup(weather.content)
+   s=soup.find("div",class_="Tooltip wind wind-E").text
+   h=soup.find("td",class_="p2 bR").text
+   temp=soup.find("p",class_="today-temp").text
+   return (s,temp,h)
